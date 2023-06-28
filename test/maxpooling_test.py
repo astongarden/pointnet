ffb6694953fba8a64f7f3be242c0d0aafb2330c5
astongarden <9812choi@naver.com>
2023-06-28 17:11:35 +0900

    save

---
+++ test/maxpooling_test.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+x = ([[1, 2, 3, 4, 5], [0, 4, 5, 20, 7], [2, 6, 15, 8, 9], [1, 2, 9 ,10 ,11], [1, 7, 3, 2, 13]])
+
+print(x)
+
+y = np.shape(x)
+print(y)
